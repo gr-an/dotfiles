aa94e2400218f1d2b38e720480d353ae305e8640
Andranik Grigoryan <andranik.grigoryan@goya.am>
2023-08-17 01:50:01 +0400

    Init

--- install.sh
+++ install.sh
@@ -8,25 +8,3 @@
 # 07/11/2014	Andranik Grigoryan	
 #
 #******************************************************************/
-
-# Running prerequisit script 
-chmod +x prerequisit.sh
-./prerequisit.sh
-
-# Moving aliases file to users dir
-cp .aliases ${HOME}/.aliases 
-
-echo "Enter your name: " 
-read name 
-
-# Backing up current configs 
-cp ${HOME}/.bashrc ${HOME}/.bashrc.back
-
-# Copy new config to home whit changing prompt name 
-sed -i "s/username/${name}/g" .bashrc 
-cp .bashrc ${HOME}/.bashrc 
-
-# Configuring neovim 
-cp ./nvim ${HOME}/.config/nvim -r
-
-echo "Done: Please restart your termianl"
